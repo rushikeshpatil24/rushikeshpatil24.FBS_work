5bf21ad527bd5c734b843cc6e50f1e5acce83595
Rushikesh Ravindra Patil <rushikeshpatil1417@gmail.com>
2026-07-17 00:49:31 +0530

    Display both roots in quadratic equation solution
    Added output for Root2 in the calculation of roots.

--- Core Python/Assignment/Assignment1/Q07.py
+++ Core Python/Assignment/Assignment1/Q07.py
@@ -1,18 +1,13 @@
 ### Write A program to enter  p,t,r and calculate Compound Intrest.
 
-#take input for a,b,c from user.
-
 a = float(input('Enter  Value of a:'))
 b = float(input('Enter value of b:')) 
 c = float(input('Enter Value of c:'))
  
-# perform Operation.
 D = b ** 2 - 4 * a * c
  
 Root1 = (-b +  D ** 0.5) / (2 * a)
 Root2 = (-b -  D ** 0.5) / (2 * a)
-
-# Display Output.
 
 print("Root1 is:",Root1)
 print("Root2 is:",Root2)
