1159c392db4ffa49f22fc7e4e1ecee2e37e9c1bd
Rushikesh Ravindra Patil <rushikeshpatil1417@gmail.com>
2026-07-17 00:55:34 +0530

    Refactor Q12.py to improve readability
    Removed unnecessary comments and adjusted formatting.

--- Core Python/Assignment/Assignment1/Q12.py
+++ Core Python/Assignment/Assignment1/Q12.py
@@ -1,11 +1,6 @@
 ### Find the volume of sphere.
 
-# Take input for radius.
-
 r = int(input('enter Radius:'))
 
-# Perform Operation.
-
 volume = (4/3) * 3.14 * r ** 3
-
 print('Volume of Sphere:',volume)
