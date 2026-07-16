61aaa5fb26140113d1d5269f639fcb94d7710269
Rushikesh Ravindra Patil <rushikeshpatil1417@gmail.com>
2026-07-17 00:53:07 +0530

    Refactor triangle area calculation program
    Removed unnecessary comments and fixed output display.

--- Core Python/Assignment/Assignment1/Q09.py
+++ Core Python/Assignment/Assignment1/Q09.py
@@ -1,13 +1,7 @@
 ## Write a  program to enter base and height of a triangle and find its area.
 
-#take input for base and height
-
 base = float(input('Enter Base:'))
 height = float(input('Enter Height:'))
 
-# calculate the area of tringle
-
 area = 1/2 * base * height
-
-#Display output
 print("Area of tringle:",area)
