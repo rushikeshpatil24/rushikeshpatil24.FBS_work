8fdc2e95e7fcb1cc7b3187d5072b3eedbec22569
Rushikesh Ravindra Patil <rushikeshpatil1417@gmail.com>
2026-07-17 00:54:18 +0530

    Refactor area calculation program for triangle
    Fixed indentation and removed unnecessary comments.

--- Core Python/Assignment/Assignment1/Q10.py
+++ Core Python/Assignment/Assignment1/Q10.py
@@ -1,13 +1,6 @@
 ### Write a prorgam to calculate area of equilateral triangle.
 
-# Take input for side.
-
-side = int(input('Enter side:'))
-
-# Perform Operation.
+side = int(input('Enter side:')
 
 area = (1.732 / 4) * side ** 2
-
-# Display Output
-
 print('The Area of Equilateral Triangle is:',area)
