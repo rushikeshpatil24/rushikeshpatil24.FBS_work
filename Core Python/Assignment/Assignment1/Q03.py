a6977755029663e8cbad049d7979cbb60f2c55f8
Rushikesh Ravindra Patil <rushikeshpatil1417@gmail.com>
2026-07-17 00:46:30 +0530

    Update Q03.py

--- Core Python/Assignment/Assignment1/Q03.py
+++ Core Python/Assignment/Assignment1/Q03.py
@@ -1,13 +1,8 @@
 ### Write a program to find Quotient and remainder of two number
 
-#take input for dividend and divisor
-
 Dividend = int(input('Enter Dividend:'))
 Divisor = int(input('Enter Divisor:'))
 
-#Perform Operation
-
 Q = Dividend // Divisor
 R = Divisor % Divisor
-
 print(f'Quotient is {Q} and Remainder is {R}')
