62203a56b620e2e805c1a4c3fdded5eaaa1d546c
Rushikesh Ravindra Patil <rushikeshpatil1417@gmail.com>
2026-07-17 00:52:30 +0530

    Refactor day conversion program for clarity

--- Core Python/Assignment/Assignment1/Q08.py
+++ Core Python/Assignment/Assignment1/Q08.py
@@ -1,17 +1,11 @@
 ### Write a program to convert a days into years,weaks and days.
 
-# Take input for day 
-
 days  = int(input('Enter Days:'))
 
-#perform Operation
- 
 years = days // 365
 remaining = days % 365
 weeks = remaining // 7
 days = remaining % 7
-
-# Display output
 
 print(f'Year:{years},Weeks:{weeks},Days:{days}')
 
