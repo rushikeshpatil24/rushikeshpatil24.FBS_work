72d06d5aef5e5a9cf800b94318227decf9514b18
Rushikesh Ravindra Patil <rushikeshpatil1417@gmail.com>
2026-08-10 16:57:43 +0530

    Update Q4.py

--- Core Python/Test/Test_1/Q4.py
+++ Core Python/Test/Test_1/Q4.py
@@ -4,15 +4,3 @@
 # (Note: 1. Below diagram is of two joint rooms.
 # 2. It is upper view of building.)
 
-area = float(input("Enter area of one wall: "))
-interior_cost = float(input("Enter interior painting cost:"))
-exterior_cost = float(input("Enter exterior painting cost:"))
-
-interior_total = area * interior_cost
-exterior_total = area * exterior_cost
-
-total_cost = interior_total + exterior_total
-
-print("Interior painting cost =", interior_total)
-print("Exterior painting cost =", exterior_total)
-print("Total painting cost =", total_cost)
